d3a72ef691be9cf2aca430acaf8d4153d70be5f9
Olga <olichka7891olichka@gmail.com>
2025-03-12 17:19:03 +0000

    add task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector(".login-form");
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
+  const { email, password } = form.elements;
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+    const formUser = {
+      email: emailValue,
+      password: passwordValue,
+    };
+    console.log(formUser);
+    form.reset();
+  }
+});
