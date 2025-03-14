300d6a65241a715eda97bf1916d4bc3b0115ccab
Olga <olichka7891olichka@gmail.com>
2025-03-14 23:57:41 +0000

    add task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const changeBackground = button.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundImage = "none";
+  body.style.backgroundColor = randomColor;
+  colorValue.textContent = randomColor;
+});
