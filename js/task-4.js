7712a70f0b5e0c30230d914808f373156db1475a
Olga <olichka7891olichka@gmail.com>
2025-03-18 14:49:09 +0000

    task correction

--- js/task-4.js
+++ js/task-4.js
@@ -1,17 +1,16 @@
-const loginForm = document.querySelector(".login-form");
-loginForm.addEventListener("submit", (event) => {
-  event.preventDefault();
+const registerForm = document.querySelector(".login-form");
 
-  const { email, password } = form.elements;
-  const emailValue = email.value.trim();
-  const passwordValue = password.value.trim();
+registerForm.addEventListener("submit", manualSubmit);
+
+function manualSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
   if (email === "" || password === "") {
     return alert("All form fields must be filled in");
-    const formUser = {
-      email: emailValue,
-      password: passwordValue,
-    };
-    console.log(formUser);
-    form.reset();
   }
-});
+  const newUser = { email, password };
+  form.reset();
+  console.log(newUser);
+}
